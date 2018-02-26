eb6f579579fc15c54dfe17e4091158156da95ee7
Agatalsqi <agatalsqi@gmail.com>
2018-02-26 04:02:08 +0300

    Solution 6-alpha

--- src/index.js
+++ src/index.js
@@ -1,3 +1,36 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  var allBracketPairs = "";
+  var openingBrackets = "";
+  var closingBrackets = "";
+  var sameBrackets = "";
+  var stack = [];
+
+  for (var i = 0; i < bracketsConfig.length; i++) {
+    allBracketPairs = allBracketPairs + bracketsConfig[i][0];
+    allBracketPairs = allBracketPairs + bracketsConfig[i][1];
+    if (bracketsConfig[i][0] == bracketsConfig[i][1]) {
+      sameBrackets = sameBrackets + bracketsConfig[i][0];
+    }
+    else {
+      openingBrackets = openingBrackets + bracketsConfig[i][0];
+      closingBrackets = closingBrackets + bracketsConfig[i][1];
+    }
+  }
+  for (var i = 0; i < str.length; i++) {
+    if (openingBrackets.indexOf(str[i])+1 || sameBrackets.indexOf(str[i])+1) {
+      stack.push(str[i]);
+    }
+    else {
+      if ((closingBrackets.indexOf(str[i])+1) || sameBrackets.indexOf(str[i]+1)) {
+        if (stack.length) {
+          if (openingBrackets.indexOf(stack[stack.length - 1])+1 && str[i] == closingBrackets[closingBrackets.indexOf(stack[stack.length - 1])+1]); 
+          stack.pop();
+        }
+        else {
+          return false;
+        }
+      }
+    }
+  }
+  return (!stack.length);
 }
